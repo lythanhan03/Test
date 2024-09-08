8baeb6851c4dff37f3096d36872c623df4b63cea
An Ly <An Ly@DESKTOP-E9VSLH9>
2024-09-08 14:03:01 +0700

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CHUONG TRINH TIM SO LON NHAT VA NHO NHAT TU 5 SO");
+
+            int[] numbers = new int[5];
+
+            // Nhập 5 số từ bàn phím
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Nhap so thu {i + 1}: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Tìm số lớn nhất và nhỏ nhất
+            int max = numbers[0];
+            int min = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+
+            // In ra kết quả
+            Console.WriteLine("So lon nhat la: " + max);
+            Console.WriteLine("So nho nhat la: " + min);
+
+        }
+    }// static: phương thức
+}
